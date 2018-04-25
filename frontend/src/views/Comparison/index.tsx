86ccc7d9f1352949a8b915e6937a9238b36c037c
Peter Matta <pet3r.matta@gmail.com>
2018-04-25 15:38:25 +0200

    [Frontend] Comparison page layout

--- frontend/src/views/Comparison/index.tsx
+++ frontend/src/views/Comparison/index.tsx
@@ -1,3 +1,43 @@
+//#region imports
 import * as React from 'react';
+import { Row, Col } from 'antd';
+import LangInput from 'components/LangInput';
+import View from './view';
+import Controls from './components/controls';
+//#endregion
 
-export default () => <h1>Comparison</h1>;
+export default class Controller extends React.Component {
+  private handleSubmit() {
+    console.log('Should compare.');
+  }
+
+  public render() {
+    return (
+      <View
+        Header={this.renderHeader()}
+        Controls={this.renderControls()}
+      >
+        <Row>
+          <Col span={12}>
+            <LangInput />
+          </Col>
+          <Col span={12}>
+            <LangInput />
+          </Col>
+        </Row>
+      </View>
+    );
+  }
+
+  private renderHeader() {
+    return <h1>Comparison</h1>;
+  }
+
+  private renderControls() {
+    return (
+      <Controls
+        onSubmit={this.handleSubmit.bind(this)}
+      />
+    );
+  }
+}
