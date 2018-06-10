441c921fe61494029a78bc69817e4b452da649da
Peter Matta <mattapet@fit.cvut.cz>
2018-06-10 13:41:43 +0200

    [Frontend][Refactor] Added type annotations and docs to Comparison controller and Language input components

--- frontend/src/components/LangInput/index.tsx
+++ frontend/src/components/LangInput/index.tsx
@@ -1,14 +1,24 @@
 //#region imports
 import * as React from 'react';
-import RegexInput from 'components/Forms/Regexp';
-import GrammarInput from 'components/Forms/Grammar';
-import AutomatonInput from 'components/Forms/Automaton';
+import RegexInput, { RegexpInputValue } from 'components/Forms/Regexp';
+import GrammarInput, { GrammarInputValue } from 'components/Forms/Grammar';
+import AutomatonInput, { AutomatonInputValue } from 'components/Forms/Automaton';
 import View from './view';
 //#endregion
 
 //#region Component interfaces
-interface ValueStore {
-  [key: string]: any;
+export enum InputType {
+  Automaton = 'au',
+  Grammar = 'gr',
+  Regexp = 're',
+}
+
+export type InputValue = AutomatonInputValue | GrammarInputValue | RegexpInputValue;
+
+export interface ValueStore {
+  [InputType.Automaton]: AutomatonInputValue;
+  [InputType.Grammar]: GrammarInputValue;
+  [InputType.Regexp]: RegexpInputValue;
 }
 
 export interface ControllerProps {
@@ -16,32 +26,32 @@ export interface ControllerProps {
 }
 
 interface ControllerState {
-  selected: string;
+  selected: InputType;
   values: ValueStore;
 }
 //#endregion
 
 export default class Controller extends React.Component<ControllerProps, ControllerState> {
   public state = {
-    selected: 'gr',
+    selected: InputType.Grammar,
     values: {
-      au: { header: [], body: [] },
-      gr: { nonTerms: '', terms: '', rules: '', startSymbol: '' },
-      re: ''
+      [InputType.Automaton]: { header: [], body: [] },
+      [InputType.Grammar]: { nonTerms: '', terms: '', rules: '', startSymbol: '' },
+      [InputType.Regexp]: ''
     }
   };
 
   /**
-   * Handles input type change
+   * Handles input type change.
    */
-  private handleTypeChange(selected: string) {
+  private handleTypeChange(selected: InputType) {
     this.setState({ selected }, () => this.props.onChange(this.state));
   }
 
   /**
    * Handles changes in value.
    */
-  private handleChange(value: any) {
+  private handleChange(value: InputValue) {
     this.setState({
       values: {
         ...this.state.values,
@@ -59,27 +69,35 @@ export default class Controller extends React.Component<ControllerProps, Control
     );
   }
 
+  /**
+   * Renders an input form compoennt depending on depending on which one is currently selected.
+   */
   private renderInput() {
-    let C;
     switch (this.state.selected) {
-    case 'gr':
-      C = GrammarInput;
-      break;
-    case 'au':
-      C = AutomatonInput;
-      break;
-    case 're':
-      C = RegexInput;
-      break;
-    default:
-      throw new TypeError('Unexpected input type');
+    case InputType.Automaton:
+      return (
+        <AutomatonInput
+          value={this.state.values[InputType.Automaton]}
+          onChange={this.handleChange.bind(this)}
+        />
+      );
+
+    case InputType.Grammar:
+      return (
+        <GrammarInput
+          value={this.state.values[InputType.Grammar]}
+          onChange={this.handleChange.bind(this)}
+        />
+      );
+
+    case InputType.Regexp:
+      return (
+        <RegexInput
+          value={this.state.values[InputType.Regexp]}
+          onChange={this.handleChange.bind(this)}
+        />
+      );
     }
-    return (
-      <C
-        value={this.state.values[this.state.selected]}
-        onChange={this.handleChange.bind(this)}
-      />
-    );
   }
 }
 
--- frontend/src/views/Comparison/index.tsx
+++ frontend/src/views/Comparison/index.tsx
@@ -2,7 +2,10 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Row, Col, notification } from 'antd';
-import LangInput from 'components/LangInput';
+import LangInput, {
+  InputType,
+  ValueStore
+} from 'components/LangInput';
 import View from './view';
 import Controls from './components/controls';
 import { validate } from './validation';
@@ -14,6 +17,7 @@ import {
 } from './selectors';
 //#endregion
 
+//#region Component interfaces
 export interface ControllerProps {
   result: boolean;
   meta: {
@@ -24,10 +28,22 @@ export interface ControllerProps {
   onCompare: (data: ComparisonRequest) => any;
 }
 
-class Controller extends React.Component<ControllerProps, any> {
+interface ControllerState {
+  lhs: {
+    selected: InputType;
+    values: ValueStore;
+  };
+  rhs: {
+    selected: InputType;
+    values: ValueStore;
+  }
+}
+//#endregion
+
+class Controller extends React.Component<ControllerProps, ControllerState> {
   public state = {
     lhs: {
-      selected: 'gr',
+      selected: InputType.Grammar,
       values: {
         au: { header: [], body: [] },
         gr: { nonTerms: '', terms: '', rules: '', startSymbol: '' },
@@ -35,7 +51,7 @@ class Controller extends React.Component<ControllerProps, any> {
       }
     },
     rhs: {
-      selected: 'gr',
+      selected: InputType.Grammar,
       values: {
         au: { header: [], body: [] },
         gr: { nonTerms: '', terms: '', rules: '', startSymbol: '' },
@@ -44,35 +60,64 @@ class Controller extends React.Component<ControllerProps, any> {
     }
   };
 
+  /**
+   * Handle form submission.
+   *
+   * @description Performs validation of both inputs. If validation does not any errors,
+   * the `onCompare` handler is called. If validation emits any error, the `handleSubmitError`
+   * method is called with emitted error as an arguemnt.
+   */
   private handleSubmit() {
     try {
       const lhs = validate(this.state.lhs);
       const rhs = validate(this.state.rhs);
       this.props.onCompare({ lhs, rhs });
     } catch (err) {
-      if (err instanceof ValidationError) {
-        notification.error({
-          message: err.name,
-          description: err.getMessage()
-        });
-      } else {
-        notification.error({
-          message: 'Unexpected error',
-          description: 'There was an unexpected error. Try repeating the action and/or reviewing the syntax.'
-        });
-      }
+      this.handleSubmitError(err);
     }
+  }
+
+  /**
+   * Handle form submission error.
+   *
+   * @description If passed error is an instance of a custom error (`ValidationError`),
+   * use name and description of that error to present an error notification.
+   * If the error is not of any expected type, present user with default erro name
+   * and description.
+   *
+   * @param err An error emitted during input validation process.
+   */
+  private handleSubmitError(err: Error) {
+    if (err instanceof ValidationError) {
+      this.presentError(err.name, err.getMessage());
+    } else {
+      const message = 'Unexpected error';
+      const description = 'There was an unexpected error. '
+      + 'Try repeating the anction and/or reviewing the syntax.';
+      this.presentError(message, description);
+    }
+  }
+
+  /**
+   * Presents an error with given `message` and `description`.
+   *
+   * @param message Notification message.
+   * @param description Notification description.
+   */
+  private presentError(message: string, description: string) {
+    notification.error({ message, description });
   }
 
   public componentWillReceiveProps(nextProps: ControllerProps) {
+    // Check if an error occoured.
     if (!this.props.meta.error && nextProps.meta.error) {
-      notification.error({
-        message: 'Connection error',
-        description: 'There appears to be a problem. Please try repeating the action.'
-      });
+      const message = 'Connection error';
+      const description = 'There appears to be a problem. Please try repeating the action.';
+      this.presentError(message, description);
     }
   }
 
+  //#region Rendering
   public render() {
     return (
       <View
@@ -109,6 +154,7 @@ class Controller extends React.Component<ControllerProps, any> {
       />
     );
   }
+  //#endregion
 }
 
 export default connect(
