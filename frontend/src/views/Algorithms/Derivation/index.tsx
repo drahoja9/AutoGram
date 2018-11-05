15807c17d44866e43d2d1daf22cb3e9d22143e93
kralidom <kralidom@fit.cvut.cz>
2018-11-05 10:46:20 +0100

    Add Controller abstract class and implement it in algorithm views

--- frontend/src/views/Algorithms/Derivation/index.tsx
+++ frontend/src/views/Algorithms/Derivation/index.tsx
@@ -1,13 +1,8 @@
 //#region imports
-import * as React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { notification } from 'antd';
+import { withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { RegexpInputValue } from 'components/Forms/Regexp';
-import { RouteHandler } from 'components';
-import { ValidationError } from 'lib/validate';
-import { ParseError } from 'lib/parse';
 import { DerivationRequest } from 'lib/types';
 
 import { routes } from './routes';
@@ -16,43 +11,28 @@ import {
   mapStateToProps,
   mapDispatchToProps
 } from './selectors';
+
+import Controller from 'components/AlgorithmView/Controller';
 //#endregion
 
 //#region Component interfaces
-export interface ControllerProps extends RouteComponentProps<any> {
-  meta: {
-    error: Error | null;
-    pending: boolean;
-    retrieved: boolean;
-  };
-  onSubmit: (data: DerivationRequest) => any;
-  onCancel: () => any;
-}
 
-interface ControllerState {
+interface DerivationState {
   values: RegexpInputValue;
   derivationString: string;
 }
 //#endregion
 
-class Controller extends React.Component<ControllerProps, ControllerState>{
+class DerivationController extends Controller<DerivationRequest, DerivationState>{
   public state = { values: '', derivationString: '' };
 
-  public componentdDidMount() {
-    if (this.props.location.pathname !== '/algo/der/input') {
-      this.props.history.push('/algo/der/input');
-    }
+  public get url(){
+    return '/algo/der'
   }
-
-  public componentWillReceiveProps(nextProps: ControllerProps) {
-    if (this.props.meta.pending && !nextProps.meta.pending && nextProps.meta.retrieved) {
-      if (!nextProps.meta.error) {
-        nextProps.history.push('/algo/der/result');
-      }
-    }
+  public get routes(){
+    return routes;
   }
-
-  private handleSubmit() {
+  protected handleSubmit(){
     const values = this.state.values;
     const derivationString = this.state.derivationString;
     try {
@@ -61,52 +41,10 @@ class Controller extends React.Component<ControllerProps, ControllerState>{
     } catch (err) {
       this.handleSubmitError(err);
     }
-
   }
-
-  private handleSubmitError(err: Error) {
-    if (err instanceof ValidationError) {
-      this.presentError(err.name, err.getMessage());
-
-    } else if (err instanceof ParseError) {
-      this.presentError(err.name, err.message);
-
-    } else {
-      const message = 'Unexpected error';
-      const description = 'There was an unexpected error. '
-        + 'Try repeating the anction and/or reviewing the syntax.';
-      this.presentError(message, description);
-    }
-  }
-
-  private presentError(message: string, description: string) {
-    notification.error({ message, description });
-  }
-
-  private handleNavigateBack() {
-    this.props.history.goBack();
-  }
-
-  private handleValueChange(change: ControllerState) {
-    this.setState(change);
-  }
-
-  public render() {
-    return (
-      <RouteHandler
-        routes={routes}
-        onBack={this.handleNavigateBack.bind(this)}
-        onSubmit={this.handleSubmit.bind(this)}
-        onValueChange={this.handleValueChange.bind(this)}
-        defaultValue={this.state}
-        pending={this.props.meta.pending}
-      />
-    )
-  }
-
 }
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withRouter(Controller));
+)(withRouter(DerivationController));
--- frontend/src/views/Algorithms/Minimization/index.tsx
+++ frontend/src/views/Algorithms/Minimization/index.tsx
@@ -1,13 +1,8 @@
 //#region imports
-import * as React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { notification } from 'antd';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { AutomatonInputValue } from 'components/Forms/Automaton';
-import { RouteHandler } from 'components';
-import { ValidationError } from 'lib/validate';
-import { ParseError } from 'lib/parse';
 import { DFA } from 'lib/types';
 
 import { routes } from './routes';
@@ -16,25 +11,17 @@ import {
   mapStateToProps,
   mapDispatchToProps
 } from './selectors';
+
+import Controller from 'components/AlgorithmView/Controller';
 //#endregion
 
 //#region Component interfaces
-export interface ControllerProps extends RouteComponentProps<any> {
-  meta: {
-    error: Error | null;
-    pending: boolean;
-    retrieved: boolean;
-  };
-  onSubmit: (data: DFA ) => any;
-  onCancel: () => any;
-}
-
-interface ControllerState {
+interface MinimizationState {
   values: AutomatonInputValue;
 }
 //#endregion
 
-class Controller extends React.Component<ControllerProps, ControllerState>{
+class MinimizationController extends Controller<DFA, MinimizationState>{
   public state = {
     values: {
       header: [], 
@@ -42,21 +29,14 @@ class Controller extends React.Component<ControllerProps, ControllerState>{
     }
   };
 
-  public componentdDidMount() {
-    if (this.props.location.pathname !== '/algo/min/input') {
-      this.props.history.push('/algo/min/input');
-    }
+  public get url(){
+    return '/algo/min'
+  }
+  public get routes(){
+    return routes;
   }
 
-  public componentWillReceiveProps(nextProps: ControllerProps) {
-    if (this.props.meta.pending && !nextProps.meta.pending && nextProps.meta.retrieved) {
-      if (!nextProps.meta.error) {
-        nextProps.history.push('/algo/min/result');
-      }
-    }
-  }
-
-  private handleSubmit() {
+  protected handleSubmit() {
     const values = this.state.values;
     try {
       const value = validate({values});
@@ -66,51 +46,9 @@ class Controller extends React.Component<ControllerProps, ControllerState>{
     }
 
   }
-
-  private handleSubmitError(err: Error) {
-    if (err instanceof ValidationError) {
-      this.presentError(err.name, err.getMessage());
-
-    } else if (err instanceof ParseError) {
-      this.presentError(err.name, err.message);
-
-    } else {
-      const message = 'Unexpected error';
-      const description = 'There was an unexpected error. '
-      + 'Try repeating the anction and/or reviewing the syntax.';
-      this.presentError(message, description);
-    }
-  }
-
-  private presentError(message: string, description: string) {
-    notification.error({ message, description });
-  }
-
-  private handleNavigateBack(){
-    this.props.history.goBack();
-  }
-
-  private handleValueChange(change: ControllerState) {
-    this.setState(change);
-  }
-
-  public render() {
-    return (
-      <RouteHandler
-        routes={routes}
-        onBack={this.handleNavigateBack.bind(this)}
-        onSubmit={this.handleSubmit.bind(this)}
-        onValueChange={this.handleValueChange.bind(this)}
-        defaultValue={this.state}
-        pending={this.props.meta.pending}
-      />
-    
-    )
-  }
-
 }
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withRouter(Controller));
+)(withRouter(MinimizationController));
