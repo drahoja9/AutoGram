9c44d2c34781bbe4cc22ecffb733a5ec9242061f
kralidom <kralidom@fit.cvut.cz>
2018-12-29 16:57:11 +0100

    Add basic style for result view

--- frontend/src/components/AlgorithmView/ResultView.tsx
+++ frontend/src/components/AlgorithmView/ResultView.tsx
@@ -1,11 +1,61 @@
 //#region imports
 import * as React from 'react';
-import { Layout, Row, Col } from 'antd';
+import styled from 'styled-components';
+import { Layout, Row, Col, Collapse } from 'antd';
+import { Centered } from 'components/Layout';
 
 import {
   ResultHeader as Header
 } from 'components/Layout';
 
+//#endregion
+
+//#region styled
+const SectionCol = styled(Col)`
+  height: calc(100% - 60px);
+`
+
+const SectionHeadline = styled.h2`
+  font-size: 2em;
+  color: #7D6F7F;
+`
+const SectionContainer = styled.div`
+  margin: 30px 0px;
+  padding: 10px 20px;
+  height: 100%;
+  &.left-border{
+    border-left: 1px solid #ccc;
+  }
+`
+
+const StepsContainer = styled.div`
+  margin: 0px 20px;
+  padding: 20px 0px;
+  border-top: 1px solid #ccc;
+`
+
+const StepsCollapse = styled(Collapse) `
+  background-color: rgba(0, 0, 0, 0);
+  & > .ant-collapse-item > .ant-collapse-header{
+    font-size: 2em;
+    color: #7D6F7F;
+    //padding-left: 0px;
+  }
+  & > .ant-collapse-item > .ant-collapse-header .arrow{
+    //left: 100px;
+    left: 0px;
+    //right: 16px;
+    font-size: 20px
+  }
+
+  &.ant-collapse-borderless > .ant-collapse-item{
+    border-style: none;
+  }
+`
+
+const StepsPanel = styled(Collapse.Panel) `
+  border-style: none;
+`
 //#endregion
 
 //#region Component interfaces
@@ -29,28 +79,42 @@ export default abstract class Controller<InputType, ResultType> extends React.Co
   public render() {
     return (
       <Layout>
-        <Header 
-          header={this.headline} 
-          onBack={this.props.onBack} 
+        <Header
+          header={this.headline}
+          onBack={this.props.onBack}
         />
         <Layout>
           <Row>
-            <Col span={12}>
-              {this.inputContent}
-            </Col>
-            <Col span={12}>
-              {this.resultContent}
-            </Col>
+            <SectionCol span={12}>
+              <SectionContainer>
+                <SectionHeadline>Input:</SectionHeadline>
+                {this.inputContent}
+              </SectionContainer>
+            </SectionCol>
+            <SectionCol span={12}>
+              <SectionContainer className={"left-border"}>
+                <SectionHeadline>Result:</SectionHeadline>
+                {this.resultContent}
+              </SectionContainer>
+            </SectionCol>
           </Row>
           {
             this.stepsContent !== null ?
-            <Row>
-              {this.stepsContent}
-            </Row>
-            :
-            null
+              <Row>
+                <StepsContainer>
+                  <StepsCollapse bordered={false}>
+                    <StepsPanel key={"1"} header={"Steps:"}>
+                      <Centered>
+                        {this.stepsContent}
+                      </Centered>
+                    </StepsPanel>
+                  </StepsCollapse>
+                </StepsContainer>
+              </Row>
+              :
+              null
           }
-            
+
         </Layout>
       </Layout>
     )
--- frontend/src/views/Algorithms/Minimization/result/index.tsx
+++ frontend/src/views/Algorithms/Minimization/result/index.tsx
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 import AutomatonView from 'components/Results/automaton';
 import { MinimizationResponse, DFA } from 'lib/types';
 import StepTable from 'components/Results/StepTable';
-import { Centered } from 'components/Layout';
 import { Row, Col } from 'antd';
 import StateIndicator from 'components/Results/automaton/StateIndicator'
 
@@ -53,104 +52,101 @@ class MinimizationController extends Controller<DFA, MinimizationResponse> {
   }
   protected get stepsContent() {
     return (
-      <Centered>
-        <MinStepTable>
-          <thead>
-            <tr>
-              <th>δ</th>
-              {
-                //generate header for table for original automaton
-                this.props.defaultValue.values.header.map((value: string, valueIdx: number) =>
-                  <th key={`min_table_header=${-1}.${valueIdx}`}>
-                    {this.props.defaultValue.values.header[valueIdx]}
-                  </th>
-                )
-              }
-              {
-                //generate headers for minimization steps
-                this.props.result.steps.map((step: any, stepIdx: number) =>
-                  [
-                    <th className={"double"} key={`min_table_header=-1.-1`}>{`δ${stepIdx}`}</th>
-                  ].concat(
-                    this.props.defaultValue.values.header.map((value: string, valueIdx: number) =>
-                      <th key={`min_table_header=${stepIdx}.${valueIdx}`}>
-                        {this.props.defaultValue.values.header[valueIdx]}
-                      </th>
-                    ))
-                )
-              }
-              <th className={"double"} key={`min_table_header=added`}>
-                {`δ${this.props.result.steps.length}`}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
+      <MinStepTable>
+        <thead>
+          <tr>
+            <th>δ</th>
             {
-              //loop through all original automaton states
-              this.props.defaultValue.values.body.map((automatonState: any, stateIdx: number) =>
-                <tr key={`min_table_row=${stateIdx}`}>
-                  {
-                    //print original automaton
-                    ...[
-                      <th key={`${stateIdx}.${-1}.-1`}>
-                        <Row>
-                          <Col span={12}>
-                            <StateWrapper>
-                              <StateIndicator
-                                isInitial={automatonState.isInitial}
-                                isFinal={automatonState.isFinal}
-                              />
-                            </StateWrapper>
-                          </Col>
-                          <Col span={12}>
-                            {automatonState.value}
-                          </Col>
-                        </Row>
-                      </th>
-                    ].concat(
-                      automatonState.values.map((value: string, valIdx: number) =>
-                        <td key={`${stateIdx}.-1.${valIdx}`}>{value}</td>
-                      )
-                    )
-                  }
-                  {
-                    //go through steps
-                    this.props.result.steps.map((step: any, stepIdx: number) =>
-                      [
-                        <th className={"double"} key={`${stateIdx}.${stepIdx}.${-1}`}>
-                          {
-                            step.filter((s: any) => s.state === automatonState.value).map((state: any) => state.group)
-                          }
-                        </th>
-                      ].concat(
-                        this.props.defaultValue.values.header.map((value: any, valueIdx: number) =>
-                          <td key={`${stateIdx}.${stepIdx}.${valueIdx}`}>
-                            {
-                              step.filter((s: any) => s.state === automatonState.value).map((s: any) =>
-                                s.transitions.filter((t: any) => t.input === value).map((t: any) =>
-                                  t.to
-                                )
-                              )
-                            }
-                          </td>
-                        )
-                      )
-                    )
-                  }
-                  <th className={"double"} key={`added.${stateIdx}`}>
-                    {
-                      //print the last iteration
-                      this.props.result.steps[this.props.result.steps.length - 1].filter((s: any) => s.state === automatonState.value)
-                        .map((s: any) => s.group)
-                    }
-                  </th>
-                </tr>
+              //generate header for table for original automaton
+              this.props.defaultValue.values.header.map((value: string, valueIdx: number) =>
+                <th key={`min_table_header=${-1}.${valueIdx}`}>
+                  {this.props.defaultValue.values.header[valueIdx]}
+                </th>
               )
             }
-          </tbody>
-        </MinStepTable>
-      </Centered>
-
+            {
+              //generate headers for minimization steps
+              this.props.result.steps.map((step: any, stepIdx: number) =>
+                [
+                  <th className={"double"} key={`min_table_header=-1.-1`}>{`δ${stepIdx}`}</th>
+                ].concat(
+                  this.props.defaultValue.values.header.map((value: string, valueIdx: number) =>
+                    <th key={`min_table_header=${stepIdx}.${valueIdx}`}>
+                      {this.props.defaultValue.values.header[valueIdx]}
+                    </th>
+                  ))
+              )
+            }
+            <th className={"double"} key={`min_table_header=added`}>
+              {`δ${this.props.result.steps.length}`}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            //loop through all original automaton states
+            this.props.defaultValue.values.body.map((automatonState: any, stateIdx: number) =>
+              <tr key={`min_table_row=${stateIdx}`}>
+                {
+                  //print original automaton
+                  ...[
+                    <th key={`${stateIdx}.${-1}.-1`}>
+                      <Row>
+                        <Col span={12}>
+                          <StateWrapper>
+                            <StateIndicator
+                              isInitial={automatonState.isInitial}
+                              isFinal={automatonState.isFinal}
+                            />
+                          </StateWrapper>
+                        </Col>
+                        <Col span={12}>
+                          {automatonState.value}
+                        </Col>
+                      </Row>
+                    </th>
+                  ].concat(
+                    automatonState.values.map((value: string, valIdx: number) =>
+                      <td key={`${stateIdx}.-1.${valIdx}`}>{value}</td>
+                    )
+                  )
+                }
+                {
+                  //go through steps
+                  this.props.result.steps.map((step: any, stepIdx: number) =>
+                    [
+                      <th className={"double"} key={`${stateIdx}.${stepIdx}.${-1}`}>
+                        {
+                          step.filter((s: any) => s.state === automatonState.value).map((state: any) => state.group)
+                        }
+                      </th>
+                    ].concat(
+                      this.props.defaultValue.values.header.map((value: any, valueIdx: number) =>
+                        <td key={`${stateIdx}.${stepIdx}.${valueIdx}`}>
+                          {
+                            step.filter((s: any) => s.state === automatonState.value).map((s: any) =>
+                              s.transitions.filter((t: any) => t.input === value).map((t: any) =>
+                                t.to
+                              )
+                            )
+                          }
+                        </td>
+                      )
+                    )
+                  )
+                }
+                <th className={"double"} key={`added.${stateIdx}`}>
+                  {
+                    //print the last iteration
+                    this.props.result.steps[this.props.result.steps.length - 1].filter((s: any) => s.state === automatonState.value)
+                      .map((s: any) => s.group)
+                  }
+                </th>
+              </tr>
+            )
+          }
+        </tbody>
+      </MinStepTable>
     )
   }
 }
